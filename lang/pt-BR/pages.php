579fc98a9156356935abed7a8452f719b493b143
Madson Lima <madson.lima@live.com>
2024-07-18 09:34:54 -0400

    feat(init): Create basic dashboard and lang i18n

---
+++ lang/pt-BR/pages.php
@@ -0,0 +1,25 @@
+<?php
+
+return  [
+    'common' => [
+        'title' => 'Controle Financeiro Pessoal'
+    ],
+    'welcome' => [
+        'Dashboard' => 'Painel',
+        'Login' => 'Entrar',
+        'Register' => 'Registrar',
+        'title' => 'Bem-vindo ao nosso aplicativo',
+        'description' => 'Este é um ótimo lugar para começar, então vá em frente e explore!',
+    ],
+    'dashboard' => [
+        'title' => 'Painel',
+        'description' => 'Bem-vindo ao painel de administração',
+        'welcome' => 'Bem-vindo ao painel de administração',
+        'users' => 'Usuários',
+        'roles' => 'Funções',
+        'permissions' => 'Permissões',
+        'settings' => 'Configurações',
+        'logout' => 'Sair',
+        "You're logged in!" => "Você está logado!",
+    ],
+];
